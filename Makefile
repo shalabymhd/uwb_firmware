70c954c81093e946c34accef6e4e98035238f196
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2021-10-25 11:48:36 -0400

    Add Decawave drivers.

--- Makefile
+++ Makefile
@@ -85,7 +85,8 @@ Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ctlreq.c \
 Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ioreq.c \
 Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/usbd_cdc.c \
 Core/imu/MPU9250.c \
-$(wildcard ./Utils/Src/*.c)
+$(wildcard ./Utils/Src/*.c) \
+$(wildcard ./Drivers/decadriver/*.c)
 
 # ASM sources
 ASM_SOURCES =  \
@@ -154,7 +155,8 @@ C_INCLUDES =  \
 -IDrivers/CMSIS/Device/ST/STM32F4xx/Include \
 -IDrivers/CMSIS/Include \
 -IUtils/Inc \
--ICore/imu
+-ICore/imu \
+-IDrivers/decadriver
 
 
 
--- Src/spi.c
+++ Src/spi.c
@@ -44,7 +44,8 @@ void MX_SPI1_Init(void)
   hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi1.Init.NSS = SPI_NSS_HARD_OUTPUT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32; // changed to _32 from _2.
+                                                           // not properly set-up in .ioc file?
   hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
