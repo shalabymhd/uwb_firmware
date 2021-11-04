f0abddebff2d41f95a31d78159b2acfec98ef396
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2021-11-04 17:03:47 +0000

    gpio.c edited online with Bitbucket

--- src/gpio.c
+++ src/gpio.c
@@ -95,7 +95,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin : PB9 */
   GPIO_InitStruct.Pin = GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
