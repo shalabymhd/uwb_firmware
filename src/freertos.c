765de5d2b54625813673a4a983ea7163d7f30931
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2022-01-25 11:16:29 -0500

    Check RX status at FSM frequency. Re-enable if off.

--- src/freertos.c
+++ src/freertos.c
@@ -165,7 +165,6 @@ void StartBlinking(void const *argument){
   while (1){
     HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7);
     osDelay(250);
-    dwt_rxenable(DWT_START_RX_IMMEDIATE);
   }
 } // end StartBlinking()
 
@@ -176,12 +175,20 @@ void StartUsbReceive(void const *argument){
   bool success;
   decaIrqStatus_t stat;
 
+  uint8_t reg_state;
+
   // FSM_status = 0; // setting the initial state of the FSM to be inactive
   while (1){
     stat = decamutexon();
     readUsb();
     decamutexoff(stat);
 
+    /* RX is supposed to be enabled from the interrupt task. If not, re-enable */
+    reg_state = dwt_read8bitoffsetreg(0x19, 1); // read RX status
+    if (!reg_state){
+      dwt_rxenable(DWT_START_RX_IMMEDIATE); // turn on uwb receiver
+    } 
+    
     switch (FSM_status)
     {
       case 0:
