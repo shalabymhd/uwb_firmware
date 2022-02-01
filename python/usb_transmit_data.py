a8d3580fc2c5373ff0048926f2416b7e85b2e7df
Charles Cossette <charles.cossette@mail.mcgill.ca>
2022-02-01 15:32:43 -0500

    Removed & terminator

--- python/usb_transmit_data.py
+++ python/usb_transmit_data.py
@@ -6,23 +6,18 @@ from pyuwb import UwbModule
 This script publishes a message to the USB port continuously until terminated.
 """
 
-#ser1 = serial.Serial('/dev/ttyACM0', 19200, timeout=1)\
+ser1 = serial.Serial('/dev/ttyACM0', 19200, timeout=1)\
 # ser3 = serial.Serial('/dev/ttyACM3', 19200, timeout=1)
-uwb = UwbModule("/dev/ttyACM0")
+# uwb = UwbModule("/dev/ttyACM0")
 
 while True:
     # dataToTransmit = "Hello World from the python script.\n"
     # # dataToTransmit = "Im python.\r"
-    # dataToTransmit = "C02,5,1\r"
-    # ser1.write(dataToTransmit.encode())
-    # c = ""
-    # resp = ""
-    # while c != "\r":
-    #     c = ser1.read(1).decode()
-    #     resp += c
-    # print(resp)
-    data = uwb.do_twr(5, meas_at_target=False)
-    print(data) 
+    dataToTransmit = "C02,5,0\r"
+    ser1.write(dataToTransmit.encode())
+    print(ser1.readline().decode(), end="")
+    #data = uwb.do_twr(5, meas_at_target=False)
+    #print(data) 
     time.sleep(0.01)
     # dataToTransmit = "C02,6,0\r"
     # ser3.write(dataToTransmit.encode())
--- src/core/ranging.c
+++ src/core/ranging.c
@@ -384,7 +384,7 @@ int rxTimestamps(uint64 tx_ts, uint64 rx_ts){
             char dist_str[10] = {0};
             convert_float_to_string(dist_str,distance);
             char response[20] = "R02,";
-            char suffix[] = "&\r";
+            char suffix[] = "\r";
             strcat(response, dist_str);
             strcat(response, suffix);
             usb_print(response); // TODO: will this response ever be sent without a USB command?
