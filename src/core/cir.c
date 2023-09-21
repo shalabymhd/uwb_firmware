6189e0ff8453db927ae094752c8b429495638137
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2023-09-21 12:59:24 -0400

    Add LDE's detected index in CIR

--- src/core/cir.c
+++ src/core/cir.c
@@ -14,6 +14,12 @@ int read_cir(uint8_t initiator_id, uint8_t target_id){
 	uint8 *cir; cir = (uint8 *)malloc((1+READ_SIZE*4)*sizeof(uint8));
 	int16 real;
 	int16 imag;
+
+	uint16 first_path_idx = dwt_read16bitoffsetreg(
+		RX_TIME_ID, 
+		RX_TIME_FP_INDEX_OFFSET
+	);
+
 	for(int i = 0;i<NUM_CIR_POINTS;i+=READ_SIZE)
 	{
 		dwt_readaccdata(cir, (1+READ_SIZE*4), 0 + 4*i);
@@ -21,25 +27,31 @@ int read_cir(uint8_t initiator_id, uint8_t target_id){
 		for (int j=0; j<READ_SIZE && j+i<NUM_CIR_POINTS; j++){
 			real =  (int16)cir[4*j+2] << 8 | (int16)cir[4*j+1];
 			imag =  (int16)cir[4*j+4] << 8 | (int16)cir[4*j+3];
-			accum_data[j+i] = (uint32)(fmax(abs(real),abs(imag)) + fmin(abs(real),abs(imag))/4);
+			accum_data[j+i] = (uint32)(
+				fmax(abs(real),abs(imag)) + fmin(abs(real),abs(imag))/4
+			);
 		}
-		
 	}
 	free(cir);
+
 	// osDelay(1);
-	output_cir(initiator_id, target_id);
+	output_cir(initiator_id, target_id, first_path_idx);
 	return 1;
 }
 
-void output_cir(uint8_t initiator_id, uint8_t target_id){
-	char response[15];
+void output_cir(uint8_t initiator_id, uint8_t target_id, uint16_t first_path_idx){
+	char response[20];
 	char* ptr = buf;
 
 	for (int lv0=0; lv0<NUM_CIR_POINTS; lv0+=NUM_SYMBOLS){
 		ptr = pos;
 		for (int lv1=lv0; lv1<lv0+NUM_SYMBOLS && lv1<NUM_CIR_POINTS; ++lv1){
 			if (lv1 == 0) {
-				sprintf(response, "%s|%d|%d", "R10", initiator_id, target_id);
+				sprintf(
+					response, "%s|%d|%d|%u|%u",
+					"R10", initiator_id, target_id, 
+					first_path_idx/64, (first_path_idx%64)*1000/64
+				);
 				strcpy(ptr, response);
 				ptr += strlen(response);
 			}
